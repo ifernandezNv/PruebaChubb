45d8eb5cf13f1688956023ccbd455b6e39cfb44f
ifernandeznv <ifernandeznv@gmail.com>
2022-06-10 07:12:26 -0600

    Ajustes a estilos

--- JS/app.js
+++ JS/app.js
@@ -4,7 +4,7 @@ const escolaridad = document.querySelector('.escolaridad__formulario');
 const intereses = document.querySelector('.intereses__formulario');
 const avance = document.querySelector('.contenido__avance')
 const iconos = document.querySelectorAll('.contenido__icono');
-
+const fin = document.querySelector('.contenido__fin');
 let iconoActivo = 0;
 
 const divAlerta = document.querySelector('.contenido__alerta');
@@ -13,7 +13,9 @@ window.onload = ()=>{
     datosPersonales.addEventListener('submit', validarDatos);
     escolaridad.classList.add('hidden');
     intereses.classList.add('hidden');
+    fin.classList.add('hidden');
     escolaridad.addEventListener('submit', validarEscolaridad);
+    intereses.addEventListener('submit', validarIntereses);
     gsap.from(datosPersonales, {opacity: 0, duration: 1, y: -100});
 }
 
@@ -48,13 +50,16 @@ function validarEscolaridad(e){
 // Funcion para validad el formulario de Intereses
 function validarIntereses(e){
     e.preventDefault();
-    // const dwa = document.querySelector('#');
-    // const dwa = document.querySelector('#');
-    // const dwa = document.querySelector('#');
-    // if(){
-        // campos vacíos
-    // }
-    borrarClases();
+    const deportistaInput = document.querySelector('#deportista');
+    const artistaInput = document.querySelector('#artista');
+    const deporteInput = document.querySelector('#deporte');
+    if(deporteInput.value === '' || artistaInput.value === '' || deportistaInput.value === ''){
+        mostrarAlerta('error', 'Todos los campos son obligatorios');
+        return;
+    }
+    mostrarAlerta('success', 'Formulario Completado');
+    mostrarAnimacion(intereses, fin);
+    document.querySelector('.contenido__progreso').classList.add('hidden');
 }
 
 // Función que muestra las animaciones
@@ -67,14 +72,18 @@ function mostrarAnimacion(formularioActual, formularioSiguiente){
         formularioSiguiente.classList.remove('hidden');
         gsap.from(formularioSiguiente, {opacity: 0, duration: 1, y: 100});
         iconos[iconoActivo].classList.add('contenido__icono-activo');
-        avance.style.width = iconoActivo * 50 + "%";
+        if(iconoActivo === 2){
+            avance.style.width = iconoActivo * 42 + "%";
+        }else{
+            avance.style.width = iconoActivo * 50 + "%";
+        }
     }, 3000);
 }
 
 // Función que muestra alertas
 function mostrarAlerta(tipo, mensaje){
-    const errores = document.querySelectorAll('error');
-    const success = document.querySelectorAll('success');
+    const errores = document.querySelectorAll('.error');
+    const success = document.querySelectorAll('.success');
     if(errores.length === 0 && success.length === 0){
         const alerta = document.createElement('p');
         alerta.classList.add('alerta');
@@ -98,6 +107,7 @@ function borrarClases(){
     iconos.forEach((icono, idx) => {
         if(iconoActivo != 0){
             icono.classList.remove('contenido__avance');
+            icono.classList.remove('contenido__icono-activo');
         }
     })
     iconoActivo = 0;
